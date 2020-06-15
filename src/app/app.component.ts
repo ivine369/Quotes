a7e354fd5fd0b44f68f3c44badec57393c55cb66
ivine369 <ivinekemuma014@gmail.com>
2020-06-15 12:59:12 +0300

    generate it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Quotes';
+
+export class AppComponent{
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
